3d9deb01463ebd1bb05db85bd2eeccc63256c6d8
marinnegai <negaimarin@gmail.com>
2024-03-11 13:34:39 +0200

    Implemented function to draw a pyramid pattern

--- GitApp/GitApp/Program.cs
+++ GitApp/GitApp/Program.cs
@@ -11,6 +11,26 @@ namespace GitApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            DrawPyramid(5);
+        }
+
+        static void DrawPyramid(int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < height - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Draw asterisks
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(); 
+            }
         }
     }
 }
